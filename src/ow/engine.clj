353155181a15f8014ec5172bd943ffc770f463ae
Nevena <nevenavv@gmail.com>
2011-04-18 16:38:49 -0700

    fixed LF-CRLF

--- src/ow/engine.clj
+++ src/ow/engine.clj
@@ -14,7 +14,7 @@
                                 (s/replace "-" "_")) ".clj"))]
     (with-open [rdr (PushbackReader. (BufferedReader. (FileReader. f)))]
 		  (loop [res []
-                           a (read rdr false :eof)]
+	 	 	   a (read rdr false :eof)]
 		     (if (= a :eof)
                         res
 		        (recur (conj res a) (read rdr false :eof)))))))
@@ -38,7 +38,7 @@
 							   super (if-let [s (second pos)] s (:super kw-map))
                                                            restrictions (set (if-let [re (second (rest pos))] re (:restrictions kw-map)))]
 							(merge-with conj res {:concepts {:name thing-name :roles roles :super super :restrictions restrictions}
-                                                                                       :concept-names thing-name})) 
+	 	 	 	 	 	 	 	 	 	       :concept-names thing-name})) 
 						    (= 'property thing) 
 						      (let [pos (take-while (comp not keyword?) params)
                                                              kw-map (apply hash-map (drop-while (comp not keyword?) params))
@@ -52,7 +52,7 @@
 								 ;some super is in obj-properties
 								 (some (:obj-property-names res) super))
 							      (merge-with conj res {:obj-properties {:name thing-name :super super :ranges obj-prop-ranges}
-                                                                                             :obj-property-names thing-name})
+	 	 	 	 	 	 	 	 	 	 	      :obj-property-names thing-name})
 					                      (let [all-restrictions (reduce (fn [r papa] (union r (:all-restrictions (first (select #(= papa (:name %)) (:dt-properties res)))))) 
                                                                                              (set restrictions) super)]
                                                                  (merge-with conj res {:dt-properties {:name thing-name :all-restrictions all-restrictions}}))))
