c689dd186524c45a753e856790570df530567c67
Nevena <nevenavv@gmail.com>
2011-04-19 12:42:56 -0700

    fixed LF-CRLF

--- src/ow/restrictions.clj
+++ src/ow/restrictions.clj
@@ -4,17 +4,18 @@
 
 (defn of-type
   [x]
-  (cond (string? x) :string 
- 			  (instance? java.lang.Integer x) :integer
-	   	  (instance? java.lang.Double x) :double
-			  (instance? java.util.Date x) :dateTime))
+  (cond 
+    (string? x) :string 
+    (instance? java.lang.Integer x) :integer
+    (instance? java.lang.Double x) :double
+    (instance? java.util.Date x) :dateTime))
 
 ;; misc --------------------------------
 (def not-nil?
   (with-meta
     (fn [prop]
       (if (= nil prop) false true))
-  {:restriction ::not-nil}))
+    {:restriction ::not-nil}))
 
 (defn has-value
   [v]
@@ -40,21 +41,21 @@
   (with-meta
     string?
     {:restriction ::type
-	   :restriction-on :string}))
+     :restriction-on :string}))
 
 (def is-int?
   (with-meta
     (fn [x]
       (instance? java.lang.Integer x))
     {:restriction ::type
-	   :restriction-on :integer}))
+     :restriction-on :integer}))
 
 (def is-double?
   (with-meta
     (fn [x]
       (instance? java.lang.Double x))
     {:restriction ::type
-	   :restriction-on :double}))
+     :restriction-on :double}))
 
 (def is-date?
   (with-meta
@@ -70,45 +71,45 @@
   (with-meta
 	  (fn [#^String s]
 	    (<= low (.length s)))
-	  {:restriction ::min-length
-	   :restriction-with low
-     :restriction-on :string}))
+   {:restriction ::min-length
+    :restriction-with low
+    :restriction-on :string}))
 
 (defn s-max-length
   [high]
   (with-meta
 	  (fn [#^String s]
 	    (>= high (.length s)))
-	  {:restriction ::max-length
-	   :restriction-with high
-     :restriction-on :string}))
+   {:restriction ::max-length
+    :restriction-with high
+    :restriction-on :string}))
 
 (defn s-length
   [v]
   (with-meta
 	  (fn [#^String s]
 	    (= v (.length s)))
-    {:restriction ::length
-     :restriction-with v
-     :restriction-on :string}))
+   {:restriction ::length
+    :restriction-with v
+    :restriction-on :string}))
 
 (defn s-length-between
   [l h]
   (with-meta
 	  (fn [#^String s]
 	     (and  (>= (.length s) l) (<= (.length s) h)))
-    {:restriction ::s-between
-     :restriction-with [l h]
-     :restriction-on :string}))
+   {:restriction ::s-between
+    :restriction-with [l h]
+    :restriction-on :string}))
 
 (defn s-pattern
   [re]
   (with-meta
 	  (fn [#^String s]
 	    (= s (re-find re s)))
-	    {:restriction ::pattern
-	     :restriction-with re ;java pattern != xsd pattern syntax!
-       :restriction-on :string}))
+   {:restriction ::pattern
+    :restriction-with re ;java pattern != xsd pattern syntax!
+    :restriction-on :string}))
 
 ;; number restrictions -----------------
 (defn lt
@@ -116,27 +117,27 @@
   (with-meta
 	  (fn [x]
 	    (< x n))
-	    {:restriction ::lt
-	     :restriction-with n
-       :restriction-on (of-type n)}))
+   {:restriction ::lt
+    :restriction-with n
+    :restriction-on (of-type n)}))
 
 (defn gt
   [n]
   (with-meta
 	  (fn [x]
 	    (> x n))
-	    {:restriction ::gt
-	     :restriction-with n
-       :restriction-on (of-type n)}))
+   {:restriction ::gt
+    :restriction-with n
+    :restriction-on (of-type n)}))
 
 (defn n-between
   [l g]
   (with-meta
 	  (fn [x]
 	    (and (> x l) (< x g)))
-	    {:restriction ::n-between
-	     :restriction-with [l g]
-       :restriction-on (of-type l)}))
+   {:restriction ::n-between
+    :restriction-with [l g]
+    :restriction-on (of-type l)}))
 
 ;; dateTime restrictions --------------------
 (defn before
@@ -144,27 +145,27 @@
   (with-meta
 	  (fn [#^java.util.Date d]
 	    (. d before t))
-	    {:restriction ::before
-	     :restriction-with t
-       :restriction-on :dateTime}))
+   {:restriction ::before
+    :restriction-with t
+    :restriction-on :dateTime}))
 
 (defn after
   [t]
   (with-meta
 	  (fn [#^java.util.Date d]
 	    (. d after t))
-	    {:restriction ::after
-	     :restriction-with t
-       :restriction-on :dateTime}))
+   {:restriction ::after
+    :restriction-with t
+    :restriction-on :dateTime}))
 
 (defn d-between
   [b a]
   (with-meta
 	  (fn [#^java.util.Date d]
 	    (and (. d before b) (. d after a)))
-	    {:restriction ::d-between
-	     :restriction-with [b a]
-       :restriction-on :dateTime}))
+   {:restriction ::d-between
+    :restriction-with [b a]
+    :restriction-on :dateTime}))
 
 ;; prepare MP predicates - restriciton fns
 (defn switch-min-length
