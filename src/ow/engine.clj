1043280a536f7b9d3ca785a3e6bb119d4bad1b73
Nevena <nevenavv@gmail.com>
2011-04-19 12:07:40 -0700

    fixed LF-CRLF

--- src/ow/engine.clj
+++ src/ow/engine.clj
@@ -1,202 +1,70 @@
-(ns ow.IRI
-  (:require [clojure.contrib.str-utils2 :only [split contains?] :as s]))
+(ns ow.engine
+ (:use [clojure.contrib.java-utils :only [file]]
+       [clojure.set :only [select union]]
+       [ow.util :only [trim-ending-?]])
+ (:require [clojure.contrib.str-utils2 :only [replace] :as s])
+ (:import [java.io File FileReader BufferedReader PushbackReader]))
 
-(def owl "http://www.w3.org/2002/07/owl#")
-(def dc "http://purl.org/dc/elements/1.1/")
-(def xsd "http://www.w3.org/2001/XMLSchema#")
-(def rdfs "http://www.w3.org/2000/01/rdf-schema#")
-(def rdf "http://www.w3.org/1999/02/22-rdf-syntax-ns#")
 
-(defn iri-expand
-  [iri-keyword]
-  (let [val (name iri-keyword)
-       [pname local] (s/split val #":")
-       pnamesym (symbol pname)]
-    (if-not (resolve pnamesym) (throw (Exception. "Namespace not defined.")))
-    (str (eval pnamesym) local)))
-    
-;;---start of comment
-(comment
-(defmacro iri
-  [compact]
-  (let [val (name compact)
-        _ (assert (s/contains? val ":"))
-        [pname local] (s/split val #":")
-        pnamesym (symbol pname)]
-    `(do 
-       (if-not (resolve '~pnamesym) (throw (Exception. "Namespace not defined.")))
-       (intern *ns* '~compact (str ~pnamesym ~local)))))
+(defn- gather-things 
+  "Returs list of statements read from clj file of given ns." ;root dir is 'src/', so for ns one.two.three
+  [domain-ns]                                                 ;it's looking for 'src/one/two/three.clj'... [not great]
+  (let [f (file (str "src/" (-> (name domain-ns)
+                                (s/replace "." "/")
+                                (s/replace "-" "_")) ".clj"))]
+    (with-open [rdr (PushbackReader. (BufferedReader. (FileReader. f)))]
+      (loop [res []
+             a (read rdr false :eof)]
+        (if (= a :eof)
+          res
+          (recur (conj res a) (read rdr false :eof)))))))
 
-(macroexpand '(iri abc:ahaha))
+; bounded in 'assort-things'
+(def concepts) ; set of maps- {:name concept-symbol :roles [roles-expr] :super [super-symbols] :restrictions [restrictions-expr]}
+(def dt-properties) ; set of maps- {:name dt-property-symbol :all-restrictions [restrictions-expr]}
+(def obj-properties) ;set of maps- {:name obj-property-symbol :super [super-symbols] :ranges [obj-prop-range-symbol]}
 
-(map iri [
-  ;OWL 2 RDF-Based Vocabulary
-  owl:AllDifferent
-  owl:AllDisjointClasses
-  owl:AllDisjointProperties
-  owl:allValuesFrom
-  owl:annotatedProperty
-  owl:annotatedSource
-  owl:annotatedTarget
-  owl:Annotation
-  owl:AnnotationProperty
-  owl:assertionProperty
-  owl:AsymmetricProperty
-  owl:Axiom
-  owl:backwardCompatibleWith
-  owl:bottomDataProperty
-  owl:bottomObjectProperty
-  owl:cardinality
-  owl:Class
-  owl:complementOf
-  owl:DataRange
-  owl:datatypeComplementOf
-  owl:DatatypeProperty
-  owl:deprecated
-  owl:DeprecatedClass
-  owl:DeprecatedProperty
-  owl:differentFrom
-  owl:disjointUnionOf
-  owl:disjointWith
-  owl:distinctMembers
-  owl:equivalentClass
-  owl:equivalentProperty
-  owl:FunctionalProperty
-  owl:hasKey
-  owl:hasSelf
-  owl:hasValue
-  owl:imports
-  owl:incompatibleWith
-  owl:intersectionOf
-  owl:InverseFunctionalProperty
-  owl:inverseOf
-  owl:IrreflexiveProperty
-  owl:maxCardinality
-  owl:maxQualifiedCardinality
-  owl:members
-  owl:minCardinality
-  owl:minQualifiedCardinality
-  owl:NamedIndividual
-  owl:NegativePropertyAssertion
-  owl:Nothing
-  owl:ObjectProperty
-  owl:onClass
-  owl:onDataRange
-  owl:onDatatype
-  owl:oneOf
-  owl:onProperty
-  owl:onProperties
-  owl:Ontology
-  owl:OntologyProperty
-  owl:priorVersion
-  owl:propertyChainAxiom
-  owl:propertyDisjointWith
-  owl:qualifiedCardinality
-  owl:ReflexiveProperty
-  owl:Restriction
-  owl:sameAs
-  owl:someValuesFrom
-  owl:sourceIndividual
-  owl:SymmetricProperty
-  owl:targetIndividual
-  owl:targetValue
-  owl:Thing
-  owl:topDataProperty
-  owl:topObjectProperty
-  owl:TransitiveProperty
-  owl:unionOf
-  owl:versionInfo
-  owl:versionIRI
-  owl:withRestrictions
-  ;RDFS vocabulary
-  rdfs:domain
-  rdfs:range
-  rdfs:Resource
-  rdfs:Literal
-  rdfs:Datatype
-  rdfs:Class
-  rdfs:subClassOf
-  rdfs:subPropertyOf
-  rdfs:member
-  rdfs:Container
-  rdfs:ContainerMembershipProperty
-  rdfs:comment
-  rdfs:seeAlso
-  rdfs:isDefinedBy
-  rdfs:label
-  ;RDF classes
-  rdfs:Resource
-  rdfs:Literal
-  rdf:XMLLiteral
-  rdfs:Class
-  rdf:Property
-  rdfs:Datatype
-  rdf:Statement
-  rdf:Bag
-  rdf:Seq
-  rdf:Alt
-  rdfs:Container
-  rdfs:ContainerMembershipProperty
-  rdf:List
-  ;RDF properties
-  rdf:type
-  rdfs:subClassOf
-  rdfs:subPropertyOf
-  rdfs:domain
-  rdfs:range
-  rdfs:label
-  rdfs:comment
-  rdfs:member
-  rdf:first
-  rdf:rest
-  rdf:value
-  rdf:subject
-  rdf:predicate
-  rdf:object
-  ;datatypes of the OWL 2 RDF-Based Semantics
-  xsd:anyURI
-  xsd:base64Binary
-  xsd:boolean
-  xsd:byte
-  xsd:dateTime
-  xsd:dateTimeStamp
-  xsd:decimal
-  xsd:double
-  xsd:float
-  xsd:hexBinary
-  xsd:int
-  xsd:integer
-  xsd:language
-  xsd:long
-  xsd:Name
-  xsd:NCName
-  xsd:negativeInteger
-  xsd:NMTOKEN
-  xsd:nonNegativeInteger
-  xsd:nonPositiveInteger
-  xsd:normalizedString
-  rdf:PlainLiteral
-  xsd:positiveInteger
-  owl:rational
-  owl:real
-  xsd:short
-  xsd:string
-  xsd:token
-  xsd:unsignedByte
-  xsd:unsignedInt
-  xsd:unsignedLong
-  xsd:unsignedShort
-  rdf:XMLLiteral
-  ;Facets of the OWL 2 RDF-Based Semantics
-  rdf:langRange
-  xsd:length
-  xsd:maxExclusive
-  xsd:maxInclusive
-  xsd:maxLength
-  xsd:minExclusive
-  xsd:minInclusive
-  xsd:minLength
-  xsd:pattern
-])
-)
-;;---end of comment
+(defn assort-things
+  "Assort things from MP model and bound to vars."
+  [mp-model]
+  (let [assorted  (reduce (fn [res el] 
+                            (let [thing (first el)
+                                  thing-name (second el)
+                                  params (drop 2 el)]
+                              (cond 
+                                (= 'concept thing)
+                                (let [pos (take-while (comp not keyword?) params)
+                                      kw-map (apply hash-map (drop-while (comp not keyword?) params))
+                                      roles (if-let [r (first pos)] r (:roles kw-map))
+                                      super (if-let [s (second pos)] s (:super kw-map))
+                                      restrictions (set (if-let [re (second (rest pos))] re (:restrictions kw-map)))]
+                                  (merge-with conj res {:concepts {:name thing-name :roles roles :super super :restrictions restrictions}
+                                                        :concept-names thing-name})) 
+                                (= 'property thing) 
+                                (let [pos (take-while (comp not keyword?) params)
+                                      kw-map (apply hash-map (drop-while (comp not keyword?) params))
+                                      restrictions (if-let [r (first pos)] r (:restrictions kw-map))
+                                      super (if-let [s (second pos)] s (:super kw-map))
+                                      obj-prop-ranges (filter identity (map #(if-let [predicate-name (trim-ending-? (str %))] 
+                                                                               (symbol predicate-name)) 
+                                                                         restrictions))]
+                                  (if (or ;some restriction is '<consept-name>?'
+                                        (some (:concept-names res) obj-prop-ranges)
+                                        ;some super is in obj-properties
+                                        (some (:obj-property-names res) super))
+                                    (merge-with conj res {:obj-properties {:name thing-name :super super :ranges obj-prop-ranges}
+                                                          :obj-property-names thing-name})
+                                    (let [all-restrictions (reduce (fn [r papa] (union r (:all-restrictions (first (select #(= papa (:name %)) (:dt-properties res)))))) 
+                                                             (set restrictions) super)]
+                                      (merge-with conj res {:dt-properties {:name thing-name :all-restrictions all-restrictions}}))))
+                                :else res)))
+                    {:concepts #{} 
+                     :dt-properties #{}
+                     :obj-properties #{}
+                     ;helpers
+                     :concept-names #{}
+                     :obj-property-names #{}}
+                    (gather-things mp-model))]
+    (intern 'ow.engine 'concepts (:concepts assorted))
+    (intern 'ow.engine 'dt-properties (:dt-properties assorted))
+    (intern 'ow.engine 'obj-properties (:obj-properties assorted))))
