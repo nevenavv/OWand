1685176c0300f1618ca404296fafcd60c4c0a0d7
Nevena <nevenavv@gmail.com>
2011-05-04 23:53:54 +0200

    added clj-plaza dep

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "OWL Wand for Magic Potion"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [robert/hooke "1.1.0"]]
+                 [robert/hooke "1.1.0"]
+                 [plaza "0.0.5-n-SNAPSHOT"]]
   :dev-dependencies [[lein-eclipse "1.0.0"]
                      [org.uncomplicate/magicpotion "0.0.1-SNAPSHOT"]]
   :repositories {"system-scope" "file://local-lib"})
